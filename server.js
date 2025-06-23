a27acfa08fcae077f7eb8419dd003f731ddcbef4
Asanka Kavinda <akavinda016@gmail.com>
2025-06-24 03:42:08 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -75,6 +75,9 @@ app.put("/api/names/:id", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
